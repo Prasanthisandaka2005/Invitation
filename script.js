928441c7572a9d58728a9addad921ec1f1e8a94e
Prasanthisandaka2005 <prasanthisandaka2005@gmail.com>
2023-10-02 18:47:48 +0530

    added pictures

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-let brideSection = document.getElementById('bride');
-let brideBtn = document.getElementById('brideBtn');
-let home = document.getElementById('home');
-let groomSection = document.getElementById('groom');
-let groomBtn = document.getElementById('groomBtn');
-
-brideBtn.onclick = function () {
-    brideSection.classList.remove('d-none');
-    home.classList.add('d-none');
-}
-groomBtn.onclick = function () {
-    groomSection.classList.remove('d-none');
-    home.classList.add('d-none');
-    brideSection.classList.add('d-none');
-}
-
 let celebrateBtn = document.getElementById('celebrate');
 let homePage = document.getElementById('pageHome');
 
